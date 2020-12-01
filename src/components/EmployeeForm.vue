335a47773cc5382bdaf3db6e93ef28f9d4671672
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-01 11:15:21 +1100

    Added in functionality to add employees

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="employee-form">
+        <form @submit.prevent="handleSubmit">
+            <label for="">Employee Name</label>
+            <input v-model="employee.name" type="text">
+            <label for="">Employee Email</label>
+            <input v-model="employee.email" type="text">
+            <button>Add Employees</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'employee-form',
+    data(){
+        return {
+            employee: {
+                name: '',
+                email: '',
+            },
+        }
+    },
+    methods: {
+        handleSubmit(){
+            this.$emit('add:employee', this.employee)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+form{
+    margin-bottom: 2rem;
+}
+
+</style>
